fd4649f5b8146b5713987057280519f0e48c8ee1
tintinmj <anirban.nag9038541773@gmail.com>
2015-01-04 00:57:03 +0530

    added generic sql create table

---
+++ SQLQuery.sql
@@ -0,0 +1,33 @@
+﻿CREATE TABLE [dbo].[tblbooking] (
+    [bid]             INT       IDENTITY (1, 1) NOT NULL,
+    [cregid]          VARCHAR (11) NOT NULL,
+    [uemail]          VARCHAR (50) NOT NULL,
+    [bpickupcity]     VARCHAR (50) NOT NULL,
+    [bpickupaddress]  VARCHAR (50) NOT NULL,
+    [bpickuplandmark] VARCHAR (50) NULL,
+    [bpickupdate]     VARCHAR (50) NOT NULL,
+    [bpickuptime]     VARCHAR (10) NOT NULL,
+    PRIMARY KEY CLUSTERED ([bid] ASC)
+);
+
+CREATE TABLE [dbo].[tblcars] (
+    [cregid]     VARCHAR (11) NOT NULL,
+    [cmanf]      VARCHAR (50) NOT NULL,
+    [cmodel]     VARCHAR (50) NOT NULL,
+    [cseat]      INT          NOT NULL,
+    [crateperkm] FLOAT (53)   NOT NULL,
+    [crateperhr] FLOAT (53)   NOT NULL,
+    [cminhr]     INT          NOT NULL,
+    [cacornonac] VARCHAR (10) NOT NULL,
+    [cphotolink] VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([cregid] ASC)
+);
+
+CREATE TABLE [dbo].[tblusers] (
+    [uname]     VARCHAR (50) NOT NULL,
+    [umobile]   VARCHAR (10) NOT NULL,
+    [uemail]    VARCHAR (50) NOT NULL,
+    [uaddress]  VARCHAR (50) NOT NULL,
+    [upassword] VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([uemail] ASC)
+);
